c5a93a220c9ef969f9b8686278a83327c2abc7e2
sathivya <dhivyaraj0066@gmail.com>
2016-12-29 12:12:04 +0530

    Add files via upload

---
+++ digit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+char str[15]={1,2,3,4,5,6};
+int i=0,length;
+while(str[i]!='\0'){
+i++;
+length=i;
+}
+printf("\n the number of digit in string is:%d",i);
+return 0;
+}
