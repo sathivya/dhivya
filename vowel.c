b0278d035ce3a44a1c06dc5a11c3e9c4720f95ce
sathivya <dhivyaraj0066@gmail.com>
2016-12-29 09:45:40 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+char ch ={'a','e','i','o','u'};
+int a;
+printf("\n enter any character");
+scanf("%c",&a);
+if(a==ch){
+printf("\n it is vowel");
+}
+else
+    printf("\n it is a consonants");
+    }
