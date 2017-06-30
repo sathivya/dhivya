dc8f75cf4d97f16c6153f4f85bd4288b10c05ed5
sathivya <dhivyaraj0066@gmail.com>
2017-06-30 15:12:21 +0530

    Add files via upload

---
+++ hunter43.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.Scanner;
+public class hunter43 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+String s2[]=s1.split(" ");
+System.out .println(s1+"the space present in s1 is");
+System.out.println(s2+"the space reduced in s2 is");
+
+	}
+
+}
