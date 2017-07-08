a4aed02e4986ac37a6d806eb40803eceb33964ae
sathivya <dhivyaraj0066@gmail.com>
2017-07-08 11:22:29 +0530

    Add files via upload

---
+++ hunter53.java
@@ -0,0 +1,21 @@
+package dhivyaguvi;
+import java.util.Scanner;
+public class hunter53 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		 
+		   
+			for(int j=1;j<s1.length();j++)
+			{
+				System.out.print(s1.charAt(0));
+				System.out.print(s1.charAt(j));
+				System.out.println("\n");
+			}
+		}
+
+	
+
+}
