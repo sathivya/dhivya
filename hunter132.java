2ad01c5ab6dc7b77706402f11ff22adc6de017f6
sathivya <dhivyaraj0066@gmail.com>
2017-07-12 20:48:54 +0530

    Add files via upload

---
+++ hunter132.java
@@ -0,0 +1,29 @@
+package Guvi;
+import java.util.Scanner;
+public class hunter132{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the size");
+		int n=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n-i;j++)
+			{
+			System.out.print("*");
+			}
+		
+		for(int f=0;f<=0;f++)
+		{
+			System.out.print(" ");
+		}
+			for(int a=0;a<n-i;a++)
+			{
+			System.out.print("*");
+			}
+			System.out.println("\n");
+	
+		}
+
+	}
+
+}
