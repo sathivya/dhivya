613f364bc0a9299d18551ff22f553a253ed0634b
sathivya <dhivyaraj0066@gmail.com>
2017-07-19 16:13:14 +0530

    Add files via upload

--- hunter68.java
+++ hunter68.java
@@ -5,26 +5,30 @@ public class hunter68 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
-		int max=-111;
 		int n=s.nextInt();
+		int temp=0;
+		int max=-999;
+		int index=0;
+		int temp1=0;
 		int a[]=new int[n];
 		for(int i=0;i<n;i++)
 		{
 			a[i]=s.nextInt();
 		}
-int index=0;
-		for(int i=0;i<n;i++)
-		{
-			for(int j=i+1;j<n;j++)
-			{
-			if(a[i]-a[j]>max)
-			{
-			max=a[i]-a[j];
-			  index=i;
+		for(int i=0;i<n-1;i++)
+		{ 
+			temp=a[i]-a[i+1];
+			 temp1=Math.abs(temp);
+			max=0;
+			
+		    if(max<temp1){
+				max=temp1;
+			   index=i;
+			   
+		    }
 			}
-			}
-		}
-System.out.println("the maximum elements is "+max+" "+index);
+		
+   System.out.println(index);
+	
 	}
-
 }
