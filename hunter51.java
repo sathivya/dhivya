2e1f94c191d3a0b9134fac4cb423aeed35d77b7c
sathivya <dhivyaraj0066@gmail.com>
2017-08-03 11:42:39 +0530

    Add files via upload

---
+++ hunter51.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.Scanner;
+public class hunter51 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		String s2=s.next();
+		int n1=Integer.parseInt(s1);
+		int n2=Integer.parseInt(s2);
+		int n3=n1*n2;
+		String s3=String.valueOf(n3);
+		System.out.println(s3);
+
+	}
+
+}
